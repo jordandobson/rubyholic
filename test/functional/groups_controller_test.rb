1e08d14d5bfa242a001768d3f8792a1c62d274b6
Jordan Dobson <jordan.dobson@madebysquad.com>
2009-03-01 20:26:57 -0800

    Added Group Sorting Tests

--- test/functional/groups_controller_test.rb
+++ test/functional/groups_controller_test.rb
@@ -11,19 +11,43 @@ class GroupsControllerTest < ActionController::TestCase
     get :index
     assert_response :success
     assert_not_nil assigns(:groups)
+    assert_template "index"
   end
 
   test "should sort by group ascending as default" do
     get :index
     assert_response :success
-
+    assert_template "index"
     # Check Arrow was set correctly
     assert_match /#{"&#9650;"}/, @response.body
-    
     # Check that the Group sort link is selected
     assert_match /<li>\s*<b>\s*<a href=\"\/groups\?by=groups&amp;s=asc\">Group<\/a>\s*<\/b>\s*<\/li>/m, @response.body
+    # Fixtures are listed in alphabetical order
+    n = 'name'
+    expected = /#{@grps['one'][n]}.*#{@grps['two'][n]}.*#{@grps['three'][n]}.*#{@grps['four'][n]}.*#{@grps['five'][n]}.*#{@grps['six'][n]}.*#{@grps['seven'][n]}.*#{@grps['eight'][n]}.*#{@grps['nine'][n]}.*#{@grps['ten'][n]}.*/m
+    assert_match expected, @response.body
+  end
 
-    # Make sure all fixtures are listed in correct order
+  test "should sort by group descending" do
+    get :index, :s => 'desc', :by => 'groups'
+    assert_response :success
+    assert_template "index"
+    # Check Arrow was set correctly
+    assert_match /#{"&#9660;"}/, @response.body
+    # Check that the Group sort link is selected
+    assert_match /<li>\s*<b>\s*<a href=\"\/groups\?by=groups&amp;s=asc\">Group<\/a>\s*<\/b>\s*<\/li>/m, @response.body
+    # Fixtures are listed in reverse alphabetical order
+    n = 'name'
+    expected = /#{@grps['twelve'][n]}.*#{@grps['eleven'][n]}.*#{@grps['ten'][n]}.*#{@grps['nine'][n]}.*#{@grps['eight'][n]}.*#{@grps['seven'][n]}.*#{@grps['six'][n]}.*#{@grps['five'][n]}.*#{@grps['four'][n]}.*#{@grps['three'][n]}.*/m
+    assert_match expected, @response.body
+  end
+
+  test "should sort by group ascending" do
+    get :index, :s => 'asc', :by => 'groups'
+    assert_response :success
+    assert_template "index"
+    assert_match /#{"&#9650;"}/, @response.body
+    assert_match /<li>\s*<b>\s*<a href=\"\/groups\?by=groups&amp;s=asc\">Group<\/a>\s*<\/b>\s*<\/li>/m, @response.body
     n = 'name'
     expected = /#{@grps['one'][n]}.*#{@grps['two'][n]}.*#{@grps['three'][n]}.*#{@grps['four'][n]}.*#{@grps['five'][n]}.*#{@grps['six'][n]}.*#{@grps['seven'][n]}.*#{@grps['eight'][n]}.*#{@grps['nine'][n]}.*#{@grps['ten'][n]}.*/m
     assert_match expected, @response.body
